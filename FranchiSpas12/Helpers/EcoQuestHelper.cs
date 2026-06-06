d18de8772e780db4ea1000e73f6ba7418b7f5907
Eco9341 <mastronardiluca7@gmail.com>
2026-06-06 02:24:34 +0200

    First Push

---
+++ FranchiSpas12/Helpers/EcoQuestHelper.cs
@@ -0,0 +1,62 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Services;
+using WTTServerCommonLib.Helpers;
+
+namespace FranchiSPAS12.Helpers
+{
+    [Injectable]
+    public class EcoQuestHelper(DatabaseService  databaseService, ISptLogger<EcoQuestHelper> logger, QuestHelper questHelper)
+    {
+
+        // Define weapon IDs
+        // ReSharper disable InconsistentNaming
+        // ReSharper disable IdentifierTypo
+        //snipers
+        //shotguns
+        private const string FRANCHI_SPAS_12 = "6a06febcf86e3e091872c104";
+        //smg
+        //pistols
+        
+        //rifles 
+        //dmr
+        
+        // Weapon Mods
+        
+        public void ModifyQuests()
+        {
+            var quests = databaseService.GetTemplates().Quests;
+
+            // ReSharper disable CommentTypo
+            // ====================== PRAPOR QUESTS ======================
+
+            // Punisher Part 4 (59ca264786f77445a80ed044)
+            questHelper.AddWeaponsToKillCondition(quests, "59ca264786f77445a80ed044", [
+                FRANCHI_SPAS_12
+            ]);
+            
+            // Silent Caliber (5c0bc91486f7746ab41857a2)
+            questHelper.AddWeaponsToKillCondition(quests, "5c0bc91486f7746ab41857a2", [
+                FRANCHI_SPAS_12
+            ]);
+
+            // Setup (5c1234c286f77406fa13baeb)
+            questHelper.AddWeaponsToKillCondition(quests, "5c1234c286f77406fa13baeb", [
+                FRANCHI_SPAS_12
+            ]);
+            
+            // Spa Tour Part 1 (5a03153686f77442d90e2171)
+            questHelper.AddWeaponsToKillCondition(quests, "5a03153686f77442d90e2171", [
+                FRANCHI_SPAS_12
+            ]);
+            
+            // Claustrophobia (669fa3979b0ce3feae01a130)
+            questHelper.AddWeaponsToKillCondition(quests, "669fa3979b0ce3feae01a130", [
+                FRANCHI_SPAS_12
+            ]);
+            
+        }
+    }
+}
